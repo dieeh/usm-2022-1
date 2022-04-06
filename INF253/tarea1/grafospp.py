2ccc61071472aa76db3dad354bfa7371e36a4e88
Dieeh <diego.paz@hotmail.com>
2022-04-05 23:53:21 -0400

    aa

--- INF253/tarea1/grafospp.py
+++ INF253/tarea1/grafospp.py
@@ -1,6 +1,12 @@
 import re
 
-numeros = '[0-9]{1,}'
+
+numeros = '[1-9][0-9]{5,}'
 
 telefono = r'\+' + numeros
 
+id = '\#[A-Z]{2,}[0-9]{2,}'
+
+names = '[A-Z][a-z\s\-]'
+
+rut = '[1-9]{1,}[0-9]{7,}\-[0-9k]'
