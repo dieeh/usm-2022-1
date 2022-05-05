34e1d89aacd3b9873daa2270c97a11683d7710ef
Diego Paz <78840156+dieeh@users.noreply.github.com>
2022-05-05 15:36:05 -0400

    xd

--- INF253/tarea3/Tarea3LP_202004502-k/tarea/files/Persona.java
+++ INF253/tarea3/Tarea3LP_202004502-k/tarea/files/Persona.java
@@ -1,1 +1,40 @@
 package tarea.files;
+
+public abstract class Persona{
+    private String nombre;
+    private int edad;
+    private int nivel;
+    private int productividad;
+
+    public void envejecer(){
+        edad +=1;
+    }
+
+    public abstract void mejorar();
+    public abstract void trabajo_realizado();
+
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String x){
+        nombre = x;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setEdad(int x){
+        edad = x;
+    }
+    public int getNivel(){
+        return nivel;
+    }
+    public void setNivel(int x){
+        nivel = x;
+    }
+    public int getProductividad(){
+        return productividad;
+    }
+    public void setProductividad(int x){
+        productividad= x;
+    }
+}
